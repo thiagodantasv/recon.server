5dbaae2080f23783201925f00fe414f7bdf0bf73
thiagodantasv <thiagodantasv@gmail.com>
2021-04-03 23:24:34 -0300

    http requisition status order

--- classes/interfaces/httpstatus.ts
+++ classes/interfaces/httpstatus.ts
@@ -1,8 +1,8 @@
 export enum RequisitionStatus{
     Ok = 200,
     Created = 201,
-    NoContent = 204,
     Accepted = 202,
+    NoContent = 204,
     BadRequest = 400,
     Unauthorized = 401,
     Forbidden = 403,
