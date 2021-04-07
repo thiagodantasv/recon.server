42edc36880ba1331896fec60ceb78922d7fa541f
thiagodantasv <thiagodantasv@gmail.com>
2021-04-06 23:19:39 -0300

    response object changes

--- classes/response.ts
+++ classes/response.ts
@@ -1,14 +1,12 @@
 import { RequisitionStatus } from './interfaces/httpstatus';
 
 export default class Response<T>{
-    public content:{
-        data: T
-    };
+    public content:T;
     public status: RequisitionStatus;
 
     constructor(status: RequisitionStatus,data?: T){
         this.status = status;
-        this.content.data = data;
+        this.content = data;
     }
 
     formatData(formatter: Function){
