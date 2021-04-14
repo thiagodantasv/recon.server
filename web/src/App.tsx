0f0643a8991536cfefed1505c2659b7a65aa0586
thiagodantasv <thiagodantasv@gmail.com>
2021-04-13 23:42:36 -0300

    managed to get one api data and insert dynamically into one material component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,54 @@
 import React, {useState, useEffect} from 'react';
-function App() {
-  const [message, setMessage] = useState('defaul value');
-  useEffect(() => {
-    console.log('this is message');
-  }, [message]);
-  
-  
-  
-  
+import axios from "axios";
+import { Card, CardContent, Paper, Typography } from '@material-ui/core';
 
+function App() {
+  const [products, setProducts] = useState<any[]>([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/getProducts').then((response) => {
+      setProducts(response.data.data.productsList);
+    });
+  }, []);
+  
+  const productCard = (name: string, price: string, availability: number) => {
+    return(
+      <div style={{padding:5}}>
+        <Card>
+          <CardContent>
+            <Typography component='h5' align='center' gutterBottom={true}>
+              {name}
+            </Typography>
+            <Typography component='h5' align='center' gutterBottom={true}>
+              {price}
+            </Typography>
+            <Typography component='h5' align='center' gutterBottom={true}>
+              {availability}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Page header</h1>
-      </header>
-      <div id="main-div">
-        <button onClick = {() => {setMessage('setted')}}></button>
-        <p>{message}</p>
-      </div>
+      <Paper elevation={3}>
+        <header className="App-header">
+          <h1>Page header</h1>
+        </header>
+        <div style = {{display:'flex', flexDirection:'row',flexWrap:'wrap' ,padding:5, width: "100%"}}>
+          {
+            products.map((product) => {
+              let name = product.name ? product.name : "-";
+              let price = product.price ? product.price : "-";
+              let availability = product.availability ? product.availability : 0;
+              return (
+                productCard(name,price,availability)
+              );
+            })
+          }
+        </div>
+      </Paper>
     </div>
   );
 }
